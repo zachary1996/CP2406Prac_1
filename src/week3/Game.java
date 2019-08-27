fc8c066c5fdc18a5d9869ae21cfab2617b699f8b
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-08-27 21:36:45 +1000

    makeGuess() method test passed

--- src/week3/Game.java
+++ src/week3/Game.java
@@ -6,10 +6,12 @@ public class Game {
     int wins;
     int count;
     private Random random = new Random();
-    //secret is assign a random value between 1-10 inclusive//
-    int secret = random.nextInt(10) + 1;
+    int secret = random.nextInt(10) + 1; //secret is assign a random value between 1-10 inclusive//
 
     void makeGuess(int value) {
-
+        count = count + 1;
+        if (value == secret) {
+            wins = wins + 1;
+        }
     }
 }
