3c2032c8a1c158a9502cdfb5a20ecb813fe29fe7
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-09-11 17:40:28 +1000

    completed prac 4 debugging tasks 1-4

---
+++ src/week5/DebugFive3.java
@@ -0,0 +1,33 @@
+// DebugFive3.java
+// Determines whether item number on order is valid
+// Over 999 invalid
+// Less than 111 Invalid
+// Valid and less than 500 - Automotive Department
+// Valid and 500 or higher Housewares Department
+package week5;
+
+import java.util.Scanner;
+
+public class DebugFive3 {
+    public static void main(String[] args) {
+        int item;
+        String output;
+        final int LOW = 999;
+        final int HIGH = 1111;
+        final int CUTOFF = 500;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter item number");
+        item = input.nextInt();
+        if (item <= CUTOFF)
+            output = "Item number too low";
+        else if (item <= LOW)
+            output = "Item number too low";
+        else if (item >= HIGH)
+            output = "Valid - in Automotive Department";
+        else
+            output = "Valid - Item in Housewares Department";
+        System.out.println(output);
+    }
+}
+
+
