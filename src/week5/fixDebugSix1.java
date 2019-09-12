8cf2d7cbd169b31d8d51e8910699bceb8b43f0a0
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-09-12 11:27:14 +1000

    debug ch 06 1 completed

---
+++ src/week5/fixDebugSix1.java
@@ -0,0 +1,18 @@
+package week5;// DebugSix1.java
+
+// Start with a penny
+// double it every day
+//how much do you have in a 30-day month?
+public class fixDebugSix1 {
+    public static void main(String[] args) {
+        final int DAYS = 30;
+        double money = 0.01;
+        int day = 1;
+        while (day < DAYS) {
+            day++;
+            money = 2 * money;
+            System.out.println("After day " + day +
+                    " you have " + money);
+        }
+    }
+}
