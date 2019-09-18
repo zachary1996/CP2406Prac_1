13424612bf0d0aea5caee99d4bb22213b7c80c32
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-09-18 10:27:36 +1000

    CarCareChoice has been updated and executes successfully without errors

--- src/week6/CarCareChoice.java
+++ src/week6/CarCareChoice.java
@@ -5,29 +5,25 @@ import javax.swing.*;
 public class CarCareChoice {
     public static void main(String[] args) {
         String entry;
-        char[] services = {'A', 'B', 'C', 'D', 'a', 'b', 'c', 'd'};
+        String[] services = {"oil change", "tire rotation", "battery check", "brake inspection"};
         int[] prices = {25, 22, 15, 5};
-        char service;
         int x, ser = 99;
         String prompt = "Please select a service\n" +
-                "Our service's are:\n" + "A - An oil change\n" +
-                "B - A tire rotation\n" +
-                "C - A battery check\n" +
-                "D - A brake inspection\n" +
-                "Enter services letter";
+                "Our service's are:\n" + "1 - oil change\n" +
+                "2 - tire rotation\n" +
+                "3 - battery check\n" +
+                "4 - brake inspection\n" +
+                "Enter service";
         entry = JOptionPane.showInputDialog(null, prompt);
-        service = entry.charAt(0);
         for (x = 0; x < services.length; ++x)
-            if (service == services[x])
+            if (entry.regionMatches(0, services[x], 0, 3))
                 ser = x;
         if (ser == 99)
             JOptionPane.showMessageDialog(null,
-                    "Invalid floor plan code entered");
+                    "Invalid service entered");
         else {
-            if (ser >= services.length / 2)
-                ser = ser - 4;
-            JOptionPane.showMessageDialog(null, "Model " +
-                    service + " is priced at only $" +
+            JOptionPane.showMessageDialog(null, "The " +
+                    services[ser] + " service is priced at $" +
                     prices[ser] + ".00");
         }
     }
