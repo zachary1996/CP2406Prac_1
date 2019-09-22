f35ed2ef95db5f9097ac0ae2187d5b54b72fc04f
zachary.sindelar <zachary.sindelar@my.jcu.edu.au>
2019-09-22 13:15:06 +1000

    debug ch 10 1 has been fixed and executes successfully without errors

---
+++ src/week7/DebugPlay.java
@@ -0,0 +1,16 @@
+package week7;
+
+public class DebugPlay {
+    protected String title;
+    protected String author;
+
+    public DebugPlay(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void display() {
+        System.out.println("The performance is " + title +
+                " by " + author);
+    }
+}
---
+++ src/week7/fixDebugTen1.java
@@ -0,0 +1,11 @@
+package week7;// A Play has a title and author
+// A Musical is a Play that also has a composer
+
+public class fixDebugTen1 {
+    public static void main(String[] args) {
+        DebugPlay performance1 = new DebugPlay("Death of a Salesman", "Arthur Miller");
+        DebugMusical performance2 = new DebugMusical("Guys and Dolls", "Jo Swerling", "Frank Loesser");
+        performance1.display();
+        performance2.display();
+    }
+}
